687fbab609efcc1d62f2e898935accbb4ace9188
Sidney-130 <janey19@DESKTOP-F726TKR.localdomain>
2025-07-23 17:08:05 +0100

    Saveing changes

--- components/waitlist.tsx
+++ components/waitlist.tsx
@@ -39,7 +39,7 @@ const Waitlist: React.FC = () => {
         </h2>
 
         {formState === "success" ? (
-          <p className="mt-6 text-lg font-medium text-neutral-500">{message}</p>
+          <p className="mt-6 text-lg font-medium text-neutralgit pull origin main --allow-unrelated-histories --no-rebase-500">{message}</p>
         ) : (
           <>
             <p className="mb-8 text-sm md:text-lg text-gray-600">
